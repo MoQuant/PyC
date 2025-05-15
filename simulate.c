48860df62484e09d7ad2fe808725e05ec0969bcd
MoQuant <112667516+MoQuant@users.noreply.github.com>
2025-05-14 23:37:20 -0400

    Update simulate.c

--- simulate.c
+++ simulate.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 
+// Generate dWT as being a number being between -10% to 10%
 double dWT()
 {
     int num = 10;
@@ -10,14 +11,16 @@ double dWT()
     return dw;
 }
 
-
+// Simulation of Geometric Brownian Motion
 double GBM(double S, double mu, double iv, double t, int n, int paths)
 {
+    // Makes sure random values are not consistent
     srand(time(NULL));
 
     double price = 0;
     double dt = t / (double) n;
 
+    // Runs all possible paths
     for(int i = 0; i < paths; ++i){
         double S0 = S;
         for(int j = 0; j < n; ++j){
@@ -26,6 +29,7 @@ double GBM(double S, double mu, double iv, double t, int n, int paths)
         price += S0;
     }
 
+    // Computes average price of simulation
     price /= (double) paths;
 
     return price;
